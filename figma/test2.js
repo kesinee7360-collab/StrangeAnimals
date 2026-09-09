e4d577de1f34e4d21a5b316aecc529f1087cd479
kesinee037 <kesinee7360@gmail.com>
2026-09-09 12:03:33 +0700

    Delete figma directory

--- figma/test2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('data.js', 'utf8');
-const regex = /"img":\s*"([^"]+)"/g;
-let match;
-const urls = [];
-while ((match = regex.exec(content)) !== null) {
-    urls.push(match[1]);
-}
-console.log(urls.slice(0, 20).join('\n'));
